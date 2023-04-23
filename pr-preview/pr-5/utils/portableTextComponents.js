5713134ea4c788db5e7087ad6a19431e85a2ef81
GlennStreetman <GlennStreetman@users.noreply.github.com>
2023-04-23 20:28:37 +0000

    Remove preview for PR 5 🛬

--- pr-preview/pr-5/utils/portableTextComponents.js
+++
@@ -1,9 +0,0 @@
-import { Link } from "@mui/material";
-
-export default {
-  marks: {
-    link: ({ children, value }) => {
-      return <Link href={value.href}>{children}</Link>;
-    },
-  },
-};
