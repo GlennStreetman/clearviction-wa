dafb62887d382a48772f692ba17a45f408c0c85b
Glenn Streetman <glennstreetman@gmail.com>
2023-04-23 14:06:22 -0700

    log base path vars

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ if (isGithubActions) {
   // @ts-ignore
   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
   const ref = process.env.NEXT_PUBLIC_PREVIEW
-
+  console.log('Repo: ', repo, " ref: ", ref)
   let assetPrefix = `/${repo}/`
   let basePath = `/${repo}/pr-preview/${ref}/`
   exportObj.assetPrefix = assetPrefix
