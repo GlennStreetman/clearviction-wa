81f2b05ee23bfcc93d823b672027c718f5aa6115
GlennStreetman <glennStreetman@gmail.com>
2023-04-23 21:25:54 +0000

    Deploying to gh-pages from @ GlennStreetman/clearviction-wa@e5c7b38802acea6dd8331a57df7d635976b0cde8 🚀

--- next.config.js
+++ next.config.js
@@ -10,9 +10,11 @@ const isGithubActions = process.env.GITHUB_ACTIONS || false
 if (isGithubActions) {
   // @ts-ignore
   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
+  let ref = process.env.NEXT_PUBLIC_PREVIEW
+  if (ref !== undefined) ref = ref.replace('refs/pull/', '').replace('/merge', '')
+  console.log('Repo: ', repo, " ref: ", ref)
   let assetPrefix = `/${repo}/`
-  let basePath = `/${repo}`
+  let basePath = `/${repo}/pr-preview/pr-${ref}`
   exportObj.assetPrefix = assetPrefix
   exportObj.basePath = basePath
   exportObj.images = {
